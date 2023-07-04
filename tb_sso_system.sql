c49a8d8b76fbb9716cde59272d2045693410887c
hn7830626 <42940041+hn7830626@users.noreply.github.com>
2023-07-04 13:49:31 +0800

    Update tb_sso_system.sql

--- tb_sso_system.sql
+++ tb_sso_system.sql
@@ -1,26 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : mysql sso
- Source Server Type    : MySQL
- Source Server Version : 80032
- Source Host           : 10.57.9.204:3306
- Source Schema         : db_sso_01
-
- Target Server Type    : MySQL
- Target Server Version : 80032
- File Encoding         : 65001
-
- Date: 04/07/2023 13:36:03
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for tb_sso_system
--- ----------------------------
-DROP TABLE IF EXISTS `tb_sso_system`;
 CREATE TABLE `tb_sso_system`  (
   `ID` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
   `CODE` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '编号',
@@ -41,5 +18,3 @@ CREATE TABLE `tb_sso_system`  (
   `REDIRECT_ROUTER` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '重定向路由',
   PRIMARY KEY (`ID`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 164 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '单点登录_应用管理' ROW_FORMAT = Dynamic;
-
-SET FOREIGN_KEY_CHECKS = 1;
